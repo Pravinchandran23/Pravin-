7ea7f865a2505cdf2be330cb2389bf53ca3e4434
Pravinchandran23 <pravinyadav002@gmail.com>
2022-02-07 16:13:32 +0530

    pravin

---
+++ src/org/com/Employee.java
@@ -0,0 +1,13 @@
+package org.com;
+
+public class Employee {
+	public void add() {
+System.out.println("addition");
+
+	}
+public static void main(String[] args) {
+	Employee e =new Employee();
+	e.add();
+	
+}
+}
